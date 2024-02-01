3379ad776bcf29526a67ab034bc1b45d950b7a38
rjsanjaymandal <rjsanjaymandal@gmail.com>
2024-02-01 08:39:25 +0000

    s

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -1,13 +1,13 @@
-const myNums = [1, 2, 3]
+// const myNums = [1, 2, 3]
 
 // const myTotal = myNums.reduce(function (acc, currval) {
 //     console.log(`acc: ${acc} and currval: ${currval}`);
 //     return acc + currval
 // }, 0)
 
-const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+// const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 
-console.log(myTotal);
+// console.log(myTotal);
 
 
 const shoppingCart = [
@@ -28,7 +28,28 @@ const shoppingCart = [
         price: 12999
     },
 ]
+//********************************************************* */
+console.log(shoppingCart.reduce((acc,item)=>acc+item.price,0));
+//********************************************************* */
 
-const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+// const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
 
-console.log(priceToPay);
+// console.log(priceToPay);
+
+// ***********************************************************
+const myNumbers=[1,2,3]
+// const myTotal = myNumbers.reduce(function (acc,curval){
+//     console.log(`acc:${acc} and curval:${curval} => ${acc+curval}`);
+//     return acc+curval
+// },0)//here zero is the initial value which is passed to the acc variable in the first iteration of the loop
+// console.log(`total => ${myTotal}`);
+
+// try the above code using arrow function
+
+// const myTotal= myNumbers.reduce((acc,curval)=>{
+//     console.log(`acc:${acc} and curval:${curval} => ${acc+curval}`);
+//     return acc+curval
+// },0);
+
+// const myTotal=console.log(myNumbers.reduce((acc,curval)=>acc+curval,0));
+// ***************************************************************
--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,10 +1,15 @@
-const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNumers.map( (num) => { return num + 10})
+// const newNums = myNumbers.map( (num) => { return num + 10})
 
-const newNums = myNumers
-                .map((num) => num * 10 )
-                .map( (num) => num + 1)
-                .filter( (num) => num >= 40)
+// const newNums = myNumers
+//                 .map((num) => num * 10 )
+//                 .map( (num) => num + 1)
+//                 .filter( (num) => num >= 40)
 
+// console.log(newNums);
+
+// ***************************************************************
+const newNums = myNumbers.map((nums) => nums + 10)
 console.log(newNums);
+// ***************************************************************
